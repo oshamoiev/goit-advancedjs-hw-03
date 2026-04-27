f088d7a122dde6321395f911d51479e9898632a9
Oleksandr Shamoiev <wamoev@gmail.com>
2026-04-27 22:40:38 +0300

    Fetch data

---
+++ src/main.js
@@ -0,0 +1,37 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+import { getImagesByQuery } from './js/pixabay-api.js';
+
+
+const form = document.querySelector('.form');
+
+const showEmptyResultNotification = () => {
+  iziToast.show({
+    message:
+      'Sorry, there are no images matching your search query. Please try again!',
+    color: 'red',
+    position: 'topRight',
+    timeout: 3000,
+  });
+};
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const { target: searchForm } = event;
+
+  const queryString = searchForm.elements['search-text'].value.trim();
+
+  getImagesByQuery(queryString)
+    .then(data => {
+      if (data.hits.length === 0) {
+        showEmptyResultNotification();
+        return;
+      }
+
+      console.log(data.hits);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+});
